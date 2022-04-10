7d5ffa082183dc395567adb879530a19d075c795
scheikh <abdulrahman404@hotmail.com>
2022-04-10 20:00:29 +0200

    Soft delete

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     use HasFactory;
 
+    use SoftDeletes;
+
     protected $fillable =['title','content'];
+
     public function comments(){
         return $this->hasMany('App\Models\Comment');
     }
